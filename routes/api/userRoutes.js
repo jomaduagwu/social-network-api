e4dc47ba405c069774ae7dcd4bdfa77aea3581c5
Ms Eko <jomaduagwu@gmail.com>
2023-06-26 00:45:26 -0500

    setup api routes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+} = require('../../controllers/userController');
+
+// /api/users
+router.route('/').get(getUsers).post(createUser);
+
+
+// /api/users/:userId
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+// /api/users/:userId/friends
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
+
+// bonus: remove a user's associated thoughts when deleted
+
+module.exports = router;
